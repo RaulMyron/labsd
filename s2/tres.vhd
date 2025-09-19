dd8caf5f4fb8541ace70a05dbb19ef248419b863
Raul Myron <raul.myron@gmail.com>
2025-09-19 17:45:58 -0300

    Update tres.vhd

--- s2/tres.vhd
+++ s2/tres.vhd
@@ -43,82 +43,69 @@ architecture ARCH of function_logic is
     signal or_100_out, or_110_a_out, or_110_b_out : std_logic;
 
 begin
-    -- Processo para conexões dos sinais
     Signal_Process: process(A, B, C, D, E, F, G)
     begin
-        decoder_in <= A & B & C & D; -- Concatenação ABCD
-        mux_select <= E & F & G; -- Seleção do multiplexador
+        decoder_in <= A & B & C & D;
+        mux_select <= E & F & G;
     end process Signal_Process;
     
-    -- Instanciação do decodificador
     decoder_inst: DECO
         port map (
             A => decoder_in,
             Y => decoder_out
         );
     
-    -- Portas OR para EFG = 000: A'B'C'D' + AB'C'D'
     or_000: OR_GATE
         port map (
-            A => decoder_out(0), -- ABCD=0000
-            B => decoder_out(8), -- ABCD=1000
+            A => decoder_out(0), 
+            B => decoder_out(8), 
             Y => func_000
         );
     
-    -- Portas OR para EFG = 001: ABCD + A'B'C'D'
     or_001: OR_GATE
         port map (
-            A => decoder_out(15), -- ABCD=1111
-            B => decoder_out(0), -- ABCD=0000
+            A => decoder_out(15), 
+            B => decoder_out(0), 
             Y => func_001
         );
     
-    -- Portas OR para EFG = 010: BCD (A=0 ou A=1)
     or_010: OR_GATE
         port map (
-            A => decoder_out(7), -- ABCD=0111
-            B => decoder_out(15), -- ABCD=1111
+            A => decoder_out(7),
+            B => decoder_out(15),
             Y => func_010
         );
     
-    -- EFG = 011: sempre 0
     func_011 <= '0';
     
-    -- Portas OR para EFG = 100: AB'C
     or_100: OR_GATE
         port map (
-            A => decoder_out(10), -- ABCD=1010
-            B => decoder_out(11), -- ABCD=1011
+            A => decoder_out(10),
+            B => decoder_out(11),
             Y => func_100
         );
     
-    -- EFG = 101: ABCD
-    func_101 <= decoder_out(15); -- ABCD=1111
+    func_101 <= decoder_out(15);
     
-    -- Portas OR para EFG = 110: AB'C + A'BCD (primeira OR)
     or_110_a: OR_GATE
         port map (
-            A => decoder_out(10), -- ABCD=1010
-            B => decoder_out(11), -- ABCD=1011
+            A => decoder_out(10), 
+            B => decoder_out(11), 
             Y => or_110_a_out
         );
     
-    -- Portas OR para EFG = 110: resultado anterior + A'BCD
     or_110_b: OR_GATE
         port map (
             A => or_110_a_out,
-            B => decoder_out(7), -- ABCD=0111
+            B => decoder_out(7), 
             Y => func_110
         );
     
-    -- EFG = 111: sempre 1 (devido a FG)
     func_111 <= '1';
     
-    -- Montagem do vetor de dados do multiplexador
     mux_data <= func_111 & func_110 & func_101 & func_100 & 
                 func_011 & func_010 & func_001 & func_000;
     
-    -- Instanciação do multiplexador
     mux_inst: MUX
         port map (
             S => mux_select,
@@ -130,7 +117,6 @@ end architecture ARCH;
 
 -----------------------------------------------------------------
 
--- Decodificador 4x16 (componente DECO)
 library IEEE;
 use IEEE.std_logic_1164.all;
 
@@ -165,7 +151,6 @@ end architecture behavioral;
 
 -----------------------------------------------------------------
 
--- Multiplexador 8x1 (componente MUX)
 library IEEE;
 use IEEE.std_logic_1164.all;
 
@@ -192,7 +177,6 @@ end architecture behavioral;
 
 -----------------------------------------------------------------
 
--- Porta OR (componente OR_GATE)
 library IEEE;
 use IEEE.std_logic_1164.all;
 
